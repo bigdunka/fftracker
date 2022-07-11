132b8a0983d274ad672dd8a3738498ad44f7295a
bigdunka <31429479+bigdunka@users.noreply.github.com>
2022-07-10 21:15:35 -0500

    2.2.3
    S8 Flag updates

--- scripts/launcher.js
+++ scripts/launcher.js
@@ -1,11 +1,11 @@
-var version = '220';
+var version = '223';
 
 function load_cookie() {
 	var c = document.cookie;
 	var cs = c.split(';');
 	
 	for (var co in cs) {
-		if (cs[co].indexOf('220') > -1 && cs[co].indexOf('settings') > -1) {
+		if (cs[co].indexOf('223') > -1 && cs[co].indexOf('settings') > -1) {
 			document.getElementById("remembersettings").checked = true;
 			document.getElementById("itemswitch").checked = false;
 			document.getElementById("locswitch").checked = false;
